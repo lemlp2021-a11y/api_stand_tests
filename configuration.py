eaf0bb2642118cd3ac25ac02292936868655884f
Luis Emilio Martínez López Padilla <lemlp2021@gmai.com>
2025-12-04 20:51:15 -0700

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-b4446f1a-45a3-4994-ba42-ee03a2cc246f.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
